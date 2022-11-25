f0619bfe90c80ac2775233c3f9283f2d5c84aee3
alecnazworth <alecnazworthalt@gmail.com>
2022-11-25 14:22:54 -0500

    Initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+  return (
+    <h4 className={styles.footer}>© 2022 Alec Nazworth. All Rights Reserved.</h4>
+  );
+}
+// export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}>
+    <nav>
+    <ul>
+        <li>
+            <Link href="/">
+                Home
+            </Link>
+        </li>
+        <li>
+            <Link href="/projects">
+                Projects
+            </Link>
+        </li>
+        <li>
+            <Link href="/contact">
+                Contact
+            </Link>
+        </li>
+      </ul>   
+    </nav>
+    </div>
+    )
+}
