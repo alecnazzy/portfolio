ca2666602f29a6c2ad4191e06f5839f1531587d0
alecnazworth <alecnazworthalt@gmail.com>
2022-12-16 13:36:20 -0500

    -updated resume -added website to In-Progress clothing store

--- components/InProgress.tsx
+++ components/InProgress.tsx
@@ -8,18 +8,24 @@ export default function InProgress() {
 
             <h1 className={home.title}>In Progress</h1>
 
-            <h1>Streetwear Online retailer</h1>
+            <h1>Online Clothing Store</h1>
             <div className={home.text}>
                 
                 
                 
-                    <p>An online clothing store project I&apos;m working on using Typescript, Next.js,
+                    <p>An online clothing store project I&apos;m working on using Next.js
                     and MongoDB.</p>
+                <h2>
+                    <Link href="https://streetwear-store.vercel.app/" target="_blank">
+                        Website
+                    </Link>
+                </h2>
                 <h2>
                     <Link href="https://github.com/alecnazzy/streetwear-store" target="_blank">
                         GitHub repository
                     </Link>
                 </h2>
+                
             </div>
         </div>
 )}
